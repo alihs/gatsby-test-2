6b95f1b2fde471d925f3ee2f2522a24df16e8d10
alijiwani <ali.jiwani@securiti.ai>
2022-02-28 10:08:53 +0500

    fixed cookie consent

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,20 @@
  */
 
 // You can delete this file if you're not using it
+export const onClientEntry = () => {
+    window.onload = () => {
+        (function() {
+            var s = document.createElement('script');
+            s.src = 'https://cdn-prod.securiti.ai/consent/cookie-consent-sdk.js';
+            s.setAttribute('data-tenant-uuid', 'ac148030-769c-49c3-95d9-312b84bb140d');
+            s.setAttribute('data-domain-uuid', '552e3395-4ef0-43a2-aa73-5b88837093d7');
+            s.setAttribute('data-backend-url', 'https://app.securiti.ai');
+            s.defer = true;
+            var parent_node = document.head || document.body;
+            parent_node.appendChild(s);
+            s.addEventListener('load', function() {
+                window.initCmp();
+            });
+        })()
+    }
+}
