67158d6820ecbe6546b4e4e3bd568dd66519a26b
alijiwani <ali.jiwani@securiti.ai>
2022-02-28 09:25:37 +0500

    added cookie pref

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,9 +41,26 @@ const Layout = ({ children }) => {
         >
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://www.gatsbyjs.com">Gatsby</a> |    <a href="javascript:void(0)" class="cmp-revoke-consent">Revoke Consent</a>
         </footer>
       </div>
+
+      <link rel='stylesheet' type='text/css' href='https://cdn-prod.securiti.ai/consent/cookie-consent.css' />
+      <script>
+      (function () {
+        var s = document.createElement('script');
+        s.src = 'https://cdn-prod.securiti.ai/consent/cookie-consent-sdk.js';
+        s.setAttribute('data-tenant-uuid', 'ac148030-769c-49c3-95d9-312b84bb140d');
+        s.setAttribute('data-domain-uuid', '552e3395-4ef0-43a2-aa73-5b88837093d7');
+        s.setAttribute('data-backend-url', 'https://app.securiti.ai');
+        s.defer = true;
+        var parent_node = document.head || document.body;
+        parent_node.appendChild(s);
+        s.addEventListener('load', function() { window.initCmp(); });
+      })()
+      </script>
+
+
     </>
   )
 }
